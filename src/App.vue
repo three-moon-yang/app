cc2b2085febfae65dd4e5bb5a5142cdd3d2b921b
17631564393 <1036032997@qq.com>
2024-12-11 22:39:08 +0800

    Vue尚品汇搭建

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
-    我是根组件
+    <Header></Header>
+    <!-- 路由组件出口 -->
+    <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
   name: 'App',
-  components: {}
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
 
 <style>
+  
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+//引入路由
+import router from '@/router'
 new Vue({
   render: h => h(App),
+  //注册路由
+  router
 }).$mount('#app')
